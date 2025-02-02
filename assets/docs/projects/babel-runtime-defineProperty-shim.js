458c1f860cb212cf3ec4d21fbac357b8fc1c56c1
oshane <othomas2@illinois.edu>
2025-02-01 18:11:51 -0800

    Update Babel runtime defineProperty shim with ES2019 target

--- assets/docs/projects/babel-runtime-defineProperty-shim.js
+++ assets/docs/projects/babel-runtime-defineProperty-shim.js
@@ -1,3 +1,3 @@
 // babel-runtime-defineProperty-shim.js
-import * as mod from "https://esm.sh/@babel/runtime@7.20.0/helpers/defineProperty.js";
+import * as mod from "https://esm.sh/@babel/runtime@7.20.0/helpers/defineProperty?target=es2019";
 export default mod;
